f0033039f318f3bc368c84f6c91bf91366b59fb4
tonishantyadav <tonishantyadav24@gmail.com>
2026-04-26 09:16:40 +0530

    Implement bean lifecycle hooks

--- src/main/java/com/nishant/store/OrderService.java
+++ src/main/java/com/nishant/store/OrderService.java
@@ -1,10 +1,22 @@
 package com.nishant.store;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 public class OrderService {
     private final PaymentService paymentService;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Init DB connection...");
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("Close DB connection...");
+    }
 
     public OrderService(@Qualifier("paypal") PaymentService paymentService) {
         this.paymentService = paymentService;
