e125caf2c4fbc4394d47568b95630a277d255596
tonishantyadav <tonishantyadav24@gmail.com>
2026-05-07 19:53:10 +0530

    Setup lombok

--- src/main/java/com/nishant/store/StoreApplication.java
+++ src/main/java/com/nishant/store/StoreApplication.java
@@ -1,21 +1,17 @@
 package com.nishant.store;
 
-import org.springframework.boot.SpringApplication;
+import com.nishant.store.entities.User;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class StoreApplication {
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-//        User user = new User();
-//        user.setId(1);
-//        user.setEmail("test@domain.com");
-//        user.setPassword("1234");
-//        user.setName("John Smith");
-//
-//        UserService userService = context.getBean(UserService.class);
-//        userService.registerUser(user);
+//        ConfigurableApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+        User user = User.builder()
+                .name("John Smith")
+                .email("johnsmith@domain.com")
+                .password("password")
+                .build();
+        System.out.println(user);
     }
 }
--- src/main/java/com/nishant/store/entities/User.java
+++ src/main/java/com/nishant/store/entities/User.java
@@ -1,7 +1,14 @@
 package com.nishant.store.entities;
 
 import jakarta.persistence.*;
+import lombok.*;
 
+@Builder
+@ToString
+@AllArgsConstructor // Custom constructor, Will not generate the default constructor
+@NoArgsConstructor //  Generate the default constructor
+@Getter
+@Setter
 @Entity
 @Table(name = "users")
 public class User {
